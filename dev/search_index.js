b3d43ab48de99a2f8a45a3aeb047b31ea69a7f93
Documenter.jl <documenter@juliadocs.github.io>
2023-02-01 23:08:42 +0000

    build based on f76525d

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Thrase","category":"page"},{"location":"#Thrase","page":"Home","title":"Thrase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Thrase. this is a test.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Thrase]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Thrase","category":"page"},{"location":"#Thrase","page":"Home","title":"Thrase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Thrase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Thrase]","category":"page"}]
 }
